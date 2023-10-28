3fa77af92701373c7d3d53ff076c4fe382e6b906
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-10-29 01:37:56 +0300

    tried to apply layers

--- src/circle_layers.R
+++ src/circle_layers.R
@@ -3,11 +3,23 @@ library(tidyverse)
 data <- tibble(x=8, y=8)
 data2 <- tibble(x=9, y=9)
 # principle of layering
-p1 <- ggplot(data = data, aes(x=x,y=y)) +
-  geom_point(pch = 21, size = 50, fill='white')
+main_plot <- ggplot(data = data, aes(x=x,y=y))
 
-p2 <- geom_point(data=data2, aes(x,y), pch = 21, size = 50, fill='white')
+x_y_deg <- function(deg) {
+  deg = deg %% 360
+  rad = deg / 360 * 2 * pi
+  c(cos(rad), sin(rad))
+}
 
-p1+p2 + coord_cartesian(xlim =c(5, 15), ylim = c(5, 15))
+start <- tibble(x=-20, y=8)
+for (deg in seq(1, 1000, 20)) {
+  data_plot <- start + x_y_deg(deg)*3
+  data_plot$x = data_plot$x + deg/40
+  data_plot$y = data_plot$y + deg/40
+  new_p <- geom_point(data=data_plot, aes(x,y),pch = 21 ,size=10, fill='white')
+  main_plot <- main_plot + new_p
+}
+
+main_plot + coord_cartesian(xlim =c(-50, 150), ylim = c(-50, 150))
 
 # gen x,y from circle and add constant move to coordinates
